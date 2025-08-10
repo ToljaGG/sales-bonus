fb095f3001bff9341c9764a09a2ac8ba41f71229
ToljaGG <toljagg123@gmail.com>
2025-08-10 21:48:23 +0700

    1

--- src/main.js
+++ src/main.js
@@ -1,44 +1,95 @@
-/**
- * Функция для расчета выручки
- * @param purchase запись о покупке
- * @param _product карточка товара
- * @returns {number}
- */
+
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+    const discount = 1 - (purchase.discount / 100);
+    return _product.sale_price * purchase.quantity * discount;
 }
 
-/**
- * Функция для расчета бонусов
- * @param index порядковый номер в отсортированном массиве
- * @param total общее число продавцов
- * @param seller карточка продавца
- * @returns {number}
- */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+    if (index === 0) {
+        return seller.profit * 0.15;
+    } else if (index === 1 || index === 2) {
+        return seller.profit * 0.10;
+    } else if (index === total - 1) {
+        return 0;
+    }
+    return seller.profit * 0.05;
 }
 
-/**
- * Функция для анализа данных продаж
- * @param data
- * @param options
- * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
- */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    if (!data
+        || !Array.isArray(data.sellers)
+        || data.sellers.length === 0
+    ) {
+        throw new Error('Некорректные входные данные');
+    }
 
-    // @TODO: Проверка наличия опций
+    const { calculateRevenue, calculateBonus } = options;
+    if (!calculateRevenue || !calculateBonus) {
+        throw new Error('Чего-то не хватает');
+    }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {},
+        top_products: [{
+            sku:"",
+            quantity: 0
+        }],
+        bonus: 0,
+    }));
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellersIndex = sellerStats.reduce((index, seller) => ({
+        ...index,
+            [seller.id]: seller
+    }), {});
+    const productsIndex = data.products.reduce((index, product) => ({
+        ...index,
+        [product.sku]: product
+    }), {})
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+    data.purchase_records.forEach(record => {
+        const seller = sellersIndex[record.seller_id];
+        seller.sales_count++;
+        seller.revenue += record.total_amount;
 
-    // @TODO: Сортировка продавцов по прибыли
+        record.items.forEach(item => {
+            const product = productsIndex[item.sku];
+            const cost = product.purchase_price * item.quantity;
+            const revenue = calculateRevenue(item, product);
+            const profit = revenue - cost;
+            seller.profit += profit;
 
-    // @TODO: Назначение премий на основе ранжирования
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+            seller.products_sold[item.sku]++;
+        })
+    });
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+    sellerStats.sort((a, b) => {
+        return b.profit - a.profit;
+    });
+
+    sellerStats.forEach((seller, index) => {
+       seller.bonus = calculateBonusByProfit(index, sellerStats.length, seller);
+       seller.top_products = Object.entries(seller.products_sold)
+           .map(([sku, quantity]) => ({ sku, quantity }))
+           .sort((a, b) => b.quantity-a.quantity)
+           .slice(0, 10);
+    });
+
+    return sellerStats.map(seller => ({
+        seller_id: seller.id,
+        name: seller.name,
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
+    }));
 }
